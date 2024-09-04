814601cb6e369d30a6dc7a8af35e1da8426765ac
marichkamt <emilycry1999@gmail.com>
2024-09-04 13:14:08 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const newul = document.createElement("ul");
+    for(const key in data){
+        const newli = document.createElement("li");
+        newli.textContent = key;
+        if (Object.keys(data[key]).length !== 0) createTree(newli, data[key]);
+        newul.append(newli);
+    }
+    element.append(newul);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
